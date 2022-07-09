7edc450838237c9c779928e0c6c85c652ddb2a95
cjhGeo <chong.jian.hang.geoffrey@sji.edu.sg>
2022-07-09 10:42:25 +0800

    Initial Commit

---
+++ Tico/TicoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicoApp.swift
+//  Tico
+//
+//  Created by T Krobot on 9/7/22.
+//
+
+import SwiftUI
+
+@main
+struct TicoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
